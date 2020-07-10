b70f43bee19748d88c8705cd6bcfab70ffb6ddcd
DERIQUEHEMMathieu <mathieuderiquehem@gmail.com>
2020-07-10 19:38:52 +0200

    Creation words list

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Creation words list
+const wordsList = ['dragon', 'flemme', 'bleu', 'coder', 'manger', 'jouer', 'dormir', 'monstre', 'console', 'boire', 'baleine', 'chat', 'dauphin',
+'huile', 'angle', 'armoire', 'banc', 'bureau', 'cabinet', 'carreau', 'chaise', 'classe', 'coin', 'couloir', 'dossier',
+'ecole', 'entrer', 'escalier', 'etagere', 'exterieur', 'fenetre', 'interieur', 'lavabo', 'marche', 'matelas', 'maternelle',
+'meuble', 'mousse', 'peluche', 'placard', 'plafond', 'porte', 'poubelle', 'radiateur', 'rampe', 'rideau', 'robinet'];
