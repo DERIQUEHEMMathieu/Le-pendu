d90a0fb3ac947a5ed47ec9dfdfb3ed92f091edeb
DERIQUEHEMMathieu <mathieuderiquehem@gmail.com>
2020-07-10 19:40:48 +0200

    Create functions

--- js/main.js
+++ js/main.js
@@ -3,3 +3,34 @@ const wordsList = ['dragon', 'flemme', 'bleu', 'coder', 'manger', 'jouer', 'dorm
 'huile', 'angle', 'armoire', 'banc', 'bureau', 'cabinet', 'carreau', 'chaise', 'classe', 'coin', 'couloir', 'dossier',
 'ecole', 'entrer', 'escalier', 'etagere', 'exterieur', 'fenetre', 'interieur', 'lavabo', 'marche', 'matelas', 'maternelle',
 'meuble', 'mousse', 'peluche', 'placard', 'plafond', 'porte', 'poubelle', 'radiateur', 'rampe', 'rideau', 'robinet'];
+
+// Functions Definition
+// Choice a word
+function computerChoose () {
+    let index = Math.floor(Math.random() * Math.floor(46));
+    let computerChoice = wordsList[index];
+    return computerChoice;
+}
+
+// Change word on array 
+function transformWord (words) {
+    let table = words.split('');
+    return table;    
+}
+
+// Change word choose to underscores
+function underscore (words) {
+    let underScore = [];
+    for (let i = 0; i < words.length; i ++){
+        underScore[i] = "_";
+    }
+    return underScore;
+}
+
+// Check if user write one and only one letter
+function checkLetter (letter) {
+    while(letter.length > 1){
+        letter = prompt("Doucement sur le clavier !" + " " + String.fromCodePoint(0x1F609) + " " + "Une seule lettre à la fois.");
+    }
+    return letter;
+}
